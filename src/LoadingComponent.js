6175264798f250e7887228fa567d251bcc112241
ArlBiern <arielbiernacki@gmail.com>
2021-11-04 14:08:10 +0100

    Create simple season app - propsa and state practice

---
+++ src/LoadingComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoadingComponent = (props) => {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui big text loader">{props.message}</div>
+    </div>
+  );
+};
+
+LoadingComponent.defaultProps = {
+  message: "Loading...",
+};
+
+export default LoadingComponent;
