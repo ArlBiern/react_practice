f0b496ac57440a6b907bbd8cb27c1ce0c9bb93c2
ArlBiern <arielbiernacki@gmail.com>
2021-11-05 11:02:55 +0100

    Correct Readme.md file

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization:
-      "Client-ID 03e6da1be322632412463af1c364abee8d88e8da45d2db5ed4ad070972fadd2f",
+    // Please add here your client ID
+    Authorization: "Client-ID Add_HERE_YOUR_CLIENT_ID",
   },
 });
