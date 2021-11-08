e5ef73dcbd6ed8e27cef0717f29742680895b548
ArlBiern <arielbiernacki@gmail.com>
2021-11-08 16:27:55 +0100

    Add dropdown widget with useRef

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./Accordion";
-import Search from "./Search";
+//import Search from "./Search";
+import Dropdown from "./Dropdown";
 
 /* const items = [
   {
@@ -17,11 +18,39 @@ import Search from "./Search";
   },
 ]; */
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      ) : null}
     </div>
   );
 };
