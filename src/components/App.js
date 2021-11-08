420e691fbd958179d6f1671a38e91be1f515c147
ArlBiern <arielbiernacki@gmail.com>
2021-11-08 14:49:20 +0100

    Add search vidget

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import Accordion from "./Accordion";
+import Search from "./Search";
+
+/* const items = [
+  {
+    title: "What is react",
+    content: "React is a front edn js library",
+  },
+  {
+    title: "Why use React",
+    content: "React is one of the favorite JS library",
+  },
+  {
+    title: "What do you use React",
+    content: "You use Ract by creating components",
+  },
+]; */
+
+const App = () => {
+  return (
+    <div>
+      {/* <Accordion items={items} /> */}
+      <Search />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: debouncedTerm,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (debouncedTerm.length > 1) {
+      search();
+    } else if (debouncedTerm.length <= 1) {
+      setResults([]);
+    }
+  }, [debouncedTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Go to article
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            className="input"
+            onChange={(e) => setTerm(e.target.value)}
+            value={term}
+          />
+        </div>
+      </div>
+      <div className="ui celled list ">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
